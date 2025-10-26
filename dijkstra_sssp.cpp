930f0be29520ffba6827063d088859257f2ae08c
Mohammad Khairul Haque <khairulhaq52@gmail.com>
2025-10-26 16:44:08 +0600

    Update dijkstra_sssp.cpp

--- dijkstra_sssp.cpp
+++ dijkstra_sssp.cpp
@@ -1,65 +1,68 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <limits.h>
 using namespace std;
 
-int minDistance(vector<int> &dist, vector<bool> &visited, int V)
+#define V 6
+
+int minDistance(int dist[], bool sptSet[])
 {
-    int minVal = INT_MAX, minIndex;
+    int min = INT_MAX, min_index;
     for (int v = 0; v < V; v++)
     {
-        if (!visited[v] && dist[v] <= minVal)
+        if (sptSet[v] == false && dist[v] <= min)
         {
-            minVal = dist[v];
-            minIndex = v;
+            min = dist[v];
+            min_index = v;
         }
     }
-    return minIndex;
+    return min_index;
 }
 
-void dijkstra(vector<vector<int>> &graph, int src)
+void printSolution(int dist[])
 {
-    int V = graph.size();
-    vector<int> dist(V, INT_MAX);
-    vector<bool> visited(V, false);
+    cout << "Vertex \t Distance from Source\n";
+    for (int i = 0; i < V; i++)
+    {
+        cout << i << " \t\t\t\t" << dist[i] << "\n";
+    }
+}
 
+void dijkstra(int graph[V][V], int src)
+{
+    int dist[V];
+    bool sptSet[V];
+    for (int i = 0; i < V; i++)
+    {
+        dist[i] = INT_MAX;
+        sptSet[i] = false;
+    }
     dist[src] = 0;
-
     for (int count = 0; count < V - 1; count++)
     {
-        int u = minDistance(dist, visited, V);
-        visited[u] = true;
-
+        int u = minDistance(dist, sptSet);
+        sptSet[u] = true;
         for (int v = 0; v < V; v++)
         {
-            if (!visited[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
             {
                 dist[v] = dist[u] + graph[u][v];
             }
         }
     }
-
-    cout << "Vertex \tDistance from Source\n";
-    for (int i = 0; i < V; i++)
-    {
-        cout << i << "\t" << dist[i] << "\n";
-    }
+    printSolution(dist);
 }
 
 int main()
 {
-    vector<vector<int>> graph =
+    int graph[V][V] =
     {
-        {0, 4, 0, 0, 0, 0, 0, 8, 0},
-        {4, 0, 8, 0, 0, 0, 0, 11, 0},
-        {0, 8, 0, 7, 0, 4, 0, 0, 2},
-        {0, 0, 7, 0, 9, 14, 0, 0, 0},
-        {0, 0, 0, 9, 0, 10, 0, 0, 0},
-        {0, 0, 4, 14, 10, 0, 2, 0, 0},
-        {0, 0, 0, 0, 0, 2, 0, 1, 6},
-        {8, 11, 0, 0, 0, 0, 1, 0, 7},
-        {0, 0, 2, 0, 0, 0, 6, 7, 0}
+        { 0, 5, 3, 0, 0, 0 },
+        { 5, 1, 1, 2, 0, 0 },
+        { 3, 1, 0, 4, 0, 0 },
+        { 0, 2, 4, 0, 2, 0 },
+        { 0, 0, 0, 2, 0, 6 },
+        { 0, 0, 0, 0, 6, 0 }
     };
-    int src = 0;
-    cout << "Dijkstra's Shortest Path from Source " << src << ":\n";
-    dijkstra(graph, src);
+    dijkstra(graph, 0);
     return 0;
 }
